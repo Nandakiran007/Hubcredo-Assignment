9d6e177c4cb9d660536f0d6737f96605af7485ff
Nandakiran007 <114251042+Nandakiran007@users.noreply.github.com>
2025-12-07 13:51:02 +0530

    cors permission

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
     cors({
-        origin: "https://hubcredo-assignment-k91i.vercel.app",
+        origin: [
+      "https://hubcredo-assignment-k91i.vercel.app", 
+      "http://localhost:5173"                        
+    ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
-app.use(cors({ origin: "http://localhost:5173" }));
+
 app.use(express.json());
 
 const authRoutes = require("./routes/auth");
